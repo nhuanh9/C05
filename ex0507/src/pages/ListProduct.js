d6a258520c6676c7a242186a331fa7fb0706bac3
nhuanh9 <chopperlouis@gmail.com>
2024-07-05 20:30:01 +0700

    chua bai 5/7 tim kiem

--- ex0507/src/pages/ListProduct.js
+++ ex0507/src/pages/ListProduct.js
@@ -9,17 +9,33 @@ export default function ListProduct() {
             name: ''
         }
     }]);
-    useEffect(()=> {
-        axios.get('http://localhost:3000/products').then(x => {
-            setList(x.data)
+    let [cates, setCates] = useState([]);
+    let [selected, setSelected] = useState(0);
+    useEffect(() => {
+        axios.get('http://localhost:3000/categories').then(x => {
+            setCates(x.data)
         })
     }, [])
+    useEffect(()=> {
+        axios.get('http://localhost:3000/products').then(x => {
+            if(selected === 0) {
+                setList(x.data)
+            } else {
+                setList(x.data.filter(e => e.category.id == selected))
+            }
+        })
+    }, [selected])
     return (
         <>
             List Product
             {list.map(e => (
                 <h2>{e.name}, {e.price}, {e.quantity}, {e.category.name}</h2>
             ))}
+            <select onChange={(e)=>{
+                setSelected(e.target.value)
+            }}>
+                {cates.map(e => (<option value={e.id}>{e.name}</option>))}
+            </select>
         </>
     )
 }
